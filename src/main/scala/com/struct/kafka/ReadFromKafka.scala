8c853c4c9169d662076df67236353c8376349d09
HG <sushruthbharath.hg@sap.com>
2019-09-26 11:49:17 +0530

    Updated ReadFromKafka

--- src/main/scala/com/struct/kafka/ReadFromKafka.scala
+++ src/main/scala/com/struct/kafka/ReadFromKafka.scala
@@ -24,6 +24,8 @@ object ReadFromKafka {
       .format("kafka")
       .option("kafka.bootstrap.servers", "10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667")
       .option("subscribe", "str_stre")
+      .option("startingOffsets", "earliest")
+      .option("endingOffsets", "latest")
       .load()
 
     val mySchema = StructType(Array(
@@ -48,6 +50,9 @@ object ReadFromKafka {
       .option("truncate","false")
       .start()
       .awaitTermination()
+
+
+
 
       spark.stop()
   }
