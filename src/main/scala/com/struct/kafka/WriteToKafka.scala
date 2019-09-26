fdc743b9223f32c3d39c8c413a5c51b89d4332f1
HG <sushruthbharath.hg@sap.com>
2019-09-26 12:39:54 +0530

    Added Write to Kafka

---
+++ src/main/scala/com/struct/kafka/WriteToKafka.scala
@@ -0,0 +1,57 @@
+package com.struct.kafka
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
+import org.apache.spark.sql.types._
+
+object WriteToKafka {
+
+
+
+  def main(args:Array[String]): Unit ={
+    Logger.getLogger("org").setLevel(Level.ERROR)
+      val spark = SparkSession
+      .builder
+      .appName("Write-To-Kafka")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    //Subscribe the Stream From Kafka
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667")
+      .option("subscribe", "str_stre")
+      .option("startingOffsets", "earliest")
+      .load()
+
+
+
+    val mySchema = StructType(Array(
+      StructField("HSCode", IntegerType),
+      StructField("Commodity", StringType),
+      StructField("value", DoubleType),
+      StructField("country", StringType),
+      StructField("year", IntegerType)
+    ))
+
+    import org.apache.spark.sql.functions.from_json
+
+    val df1 = df.selectExpr("CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)").as[(String, Timestamp)]
+      .select(from_json($"value", mySchema).as("data"), $"timestamp")
+      .select("data.*", "timestamp")
+
+    //Write Dataframe to Kafka
+
+    df1.writeStream
+      .format("kafka")
+      .option("topic", "Struct_Streaming")
+      .option("kafka.bootstrap.servers", "10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667")
+      .option("checkpointLocation", "/home/hdfs/checkpoint")
+      .start()
+
+      spark.stop()
+  }
+}
