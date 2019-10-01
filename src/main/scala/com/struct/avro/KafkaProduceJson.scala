307d5daf04538a0137315eeb490d61982b59d66e
I504285 <bharathhg@gmail.com>
2019-10-01 15:41:25 +0530

    Added new

---
+++ src/main/scala/com/struct/avro/KafkaProduceJson.scala
@@ -0,0 +1,35 @@
+package com.struct.avro
+import org.apache.spark.sql.SparkSession
+object KafkaProduceJson {
+  def main(args:Array[String]): Unit ={
+
+
+    val spark: SparkSession = SparkSession.builder()
+      .appName("SparkByExamples.com")
+      .getOrCreate()
+
+    val data = Seq((1,"James ","","Smith",2018,1,"M",3000),
+      (2,"Michael ","Rose","",2010,3,"M",4000),
+      (3,"Robert ","","Williams",2010,3,"M",4000),
+      (4,"Maria ","Anne","Jones",2005,5,"F",4000),
+      (5,"Jen","Mary","Brown",2010,7,"",-1)
+    )
+
+    val columns = Seq("id","firstname","middlename","lastname","dob_year",
+      "dob_month","gender","salary")
+    import spark.sqlContext.implicits._
+    val df = data.toDF(columns:_*)
+
+    val ds = df.toJSON
+    ds.printSchema()
+
+    val query = ds
+      .writeStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667")
+      .option("topic", "text_topic")
+      .start()
+
+  }
+
+}
