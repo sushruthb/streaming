eb4c32a75204568927525435a40198a4ecfe3e58
I504285 <bharathhg@gmail.com>
2019-10-09 12:58:13 +0530

    update

--- src/main/scala/com/struct/kafka/WriteToKafka.scala
+++ src/main/scala/com/struct/kafka/WriteToKafka.scala
@@ -4,7 +4,7 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
 import org.apache.spark.sql.types._
-import com.struct.kafka.KafkaSink
+import com.struct.kafka._
 
 object WriteToKafka {
 
@@ -55,20 +55,20 @@ object WriteToKafka {
     val brokers = "10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667"
 
     val writer = new KafkaSink(topic, brokers)
+    /*
+        val query = df1
+          .writeStream
+          .foreach(writer )
+          .outputMode("update")
+          .start()
 
-    val query = df1
-      .writeStream
-      .foreach(writer )
-      .outputMode("update")
-      .start()
-
-/*    val ds = df1
-      .writeStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667")
-      .option("topic", "Struct_Streaming")
-      .start()
-      .awaitTermination()*/
+       val ds = df1
+          .writeStream
+          .format("kafka")
+          .option("kafka.bootstrap.servers", "10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667")
+          .option("topic", "Struct_Streaming")
+          .start()
+          .awaitTermination()*/
 
       spark.stop()
   }
