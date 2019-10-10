0adf1234ea91fc11c1ee76605ff4fcee820dc7c2
I504285 <bharathhg@gmail.com>
2019-10-10 14:50:34 +0530

    update

--- src/main/scala/com/struct/kafka/KafkaConsumerSubscribeApp.scala
+++ src/main/scala/com/struct/kafka/KafkaConsumerSubscribeApp.scala
@@ -17,7 +17,7 @@ object KafkaConsumerSubscribeApp extends App {
   props.put("enable.auto.commit", "true")
   props.put("auto.commit.interval.ms", "1000")
   val consumer = new KafkaConsumer(props)
-  val topics = List("topic_text")
+  val topics = List("topic_txt")
   try {
     consumer.subscribe(topics.asJava)
     //consumer.subscribe(Collections.singletonList("topic_partition"))
