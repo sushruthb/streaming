90adb10a4fbf843018c354e4bd9b3b077f297a01
I504285 <bharathhg@gmail.com>
2019-10-10 14:39:37 +0530

    update

---
+++ src/main/scala/com/struct/kafka/KafkaConsumerSubscribeApp.scala
@@ -0,0 +1,37 @@
+package com.struct.kafka
+
+import java.util.{Collections, Properties}
+import java.util.regex.Pattern
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.log4j.{Level, Logger}
+
+import scala.collection.JavaConverters._
+object KafkaConsumerSubscribeApp extends App {
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val props:Properties = new Properties()
+  props.put("group.id", "test")
+  props.put("bootstrap.servers","10.76.106.229:6667,10.76.107.133:6667,10.76.117.167:6667")
+  props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("enable.auto.commit", "true")
+  props.put("auto.commit.interval.ms", "1000")
+  val consumer = new KafkaConsumer(props)
+  val topics = List("topic_text")
+  try {
+    consumer.subscribe(topics.asJava)
+    //consumer.subscribe(Collections.singletonList("topic_partition"))
+    //consumer.subscribe(Pattern.compile("topic_partition"))
+    while (true) {
+      val records = consumer.poll(10)
+      for (record <- records.asScala) {
+        println("Topic: " + record.topic() + ", Key: " + record.key() + ", Value: " + record.value() +
+          ", Offset: " + record.offset() + ", Partition: " + record.partition())
+      }
+    }
+  }catch{
+    case e:Exception => e.printStackTrace()
+  }finally {
+    consumer.close()
+  }
+}
