85319e2f5402a937ed245eb6d86df5be5a431a44
Sushruth Bharath HG <bharathhg@gmail.com>
2019-11-19 18:57:48 +0530

    Updated

--- src/main/scala/com/struct/hive/HiveJdbcExample.java
+++ src/main/scala/com/struct/hive/HiveJdbcExample.java
@@ -22,7 +22,7 @@ public class HiveJdbcExample {
             System.exit(1);
         }
 
-        Connection conn = DriverManager.getConnection("jdbc:hive2://localhost:10000", "hive", "");
+        Connection conn = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", "hive", "");
         Statement stmt = conn.createStatement();
         // show tables
         String sql = "SHOW databases";
