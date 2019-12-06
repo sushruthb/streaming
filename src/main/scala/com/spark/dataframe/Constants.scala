7fcdcb884d3e0a60f9a364ab41b10bc06cd30d37
Sushruth Bharath HG <bharathhg@gmail.com>
2019-12-06 11:31:00 +0530

    updatd

--- src/main/scala/com/spark/dataframe/Constants.scala
+++ src/main/scala/com/spark/dataframe/Constants.scala
@@ -5,12 +5,12 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object Constants {
 
   val spark: SparkSession = SparkSession.builder().appName("dataframe-operations").getOrCreate()
-val resource=getClass.getResource("/retailer.csv")
+//val resource=getClass.getResource("/retailer.csv")
   val df: DataFrame = spark
     .read
     .format("csv")
     .option("header", "true")
     .option("inferSchema", "true")
-    .load(resource)
+    .load("/retailer.csv")
 //  getClass.getResourceAsStream("/data/url_list1.csv")
 }
