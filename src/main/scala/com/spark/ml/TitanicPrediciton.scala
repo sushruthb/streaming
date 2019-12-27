807eba915eb071c295b8f30795bb2c6214eefe20
I504285 <bharathhg@gmail.com>
2019-12-27 10:33:40 +0530

    Updated

---
+++ src/main/scala/com/spark/ml/TitanicPrediciton.scala
@@ -0,0 +1,14 @@
+package com.spark.ml
+
+import com.spark.streaming.LoggerHelper
+import org.apache.spark.sql.SparkSession
+object TitanicPrediciton extends LoggerHelper {
+
+  def main(args:Array[String]): Unit ={
+      val spark=SparkSession.builder().appName("ML").getOrCreate()
+
+
+
+  }
+
+}
