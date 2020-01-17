8ba0af28fc201f08b95796b0122056e5d8824ca4
I504285 <bharathhg@gmail.com>
2020-01-17 10:07:59 +0530

    Updated

---
+++ src/main/scala/com/spark/streaming/spark/ReadCSVStreaming.scala
@@ -0,0 +1,36 @@
+package com.spark.streaming.spark
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StructType
+object ReadCSVStreaming {
+  def main(args:Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName( "StructuredNetworkWordCount" )
+      .getOrCreate()
+
+    import spark.implicits._
+
+
+    val userSchema = new StructType().add("code", "integer").add("description", "string").add("SortOrder","integer")
+    val csvDF = spark
+      .readStream
+      .option("sep", ";")
+      .schema(userSchema)      // Specify schema of the csv files
+      .csv("/data/DimenLookupAge8317.csv")    // Equivalent to format("csv").load("/path/to/directory")
+
+    val words = csvDF.as[String].flatMap(_.split(" "))
+
+    // Generate running word count
+    val wordCounts = words.groupBy("value").count()
+
+    // Start running the query that prints the running counts to the console
+    val query = wordCounts.writeStream
+      .outputMode("update")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+  }
+
+}
