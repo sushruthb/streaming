90929f4bfc519ebf04cda0637b28c1906b90c6aa
I504285 <bharathhg@gmail.com>
2020-01-21 20:14:33 +0530

    Updated:

--- src/main/scala/com/spark/hive/SparkHiveExample.scala
+++ src/main/scala/com/spark/hive/SparkHiveExample.scala
@@ -31,46 +31,40 @@ object SparkHiveExample {
       sql( "drop database hive cascade" )
       sql("create database hive")
       sql("use hive")
-    }
-    if (!spark.catalog.tableExists( "src" )) {
-
-    sql( "drop table src" )
       sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING) using hive")
+      // sql("LOAD DATA INPATH '/user/hdfs/data/kv1.txt' INTO TABLE src")
       sql("load data inpath '/user/hdfs/data/kv1.txt' into table src")
-    // sql("LOAD DATA INPATH '/user/hdfs/data/kv1.txt' INTO TABLE src")
-  }
-    sql("SELECT * FROM src").show()
-    // Aggregation queries are also supported.
-    sql("SELECT COUNT(*) FROM src").show()
 
-    // The results of SQL queries are themselves DataFrames and support all normal functions.
-    val sqlDF = sql("SELECT key, value FROM src WHERE key < 10 ORDER BY key")
+      sql("SELECT * FROM src").show()
+      // Aggregation queries are also supported.
+      sql("SELECT COUNT(*) FROM src").show()
+      // The results of SQL queries are themselves DataFrames and support all normal functions.
+      val sqlDF = sql("SELECT key, value FROM src WHERE key < 10 ORDER BY key")
 
-    // The items in DataFrames are of type Row, which allows you to access each column by ordinal.
-    val stringsDS = sqlDF.map {
-      case Row(key: Int, value: String) => s"Key: $key, Value: $value"
+      // The items in DataFrames are of type Row, which allows you to access each column by ordinal.
+      val stringsDS = sqlDF.map {
+        case Row(key: Int, value: String) => s"Key: $key, Value: $value"
+      }
+      stringsDS.show()
+      // You can also use DataFrames to create temporary views within a SparkSession.
+      val recordsDF = spark.createDataFrame((1 to 100).map(i => Record(i, s"val_$i")))
+      recordsDF.createOrReplaceTempView("records")
+
+      // Queries can then join DataFrame data with data stored in Hive.
+      sql("SELECT * FROM records r JOIN src s ON r.key = s.key").show()
+
+      // `USING hive`
+      if(!spark.catalog.tableExists("hive_records")){
+        sql("drop table hive_records")
+        sql("CREATE TABLE hive_records(key int, value string) STORED AS PARQUET")
+      }
+      // Save DataFrame to the Hive managed table
+      val df = spark.table("src")
+
+      df.write.mode(SaveMode.Overwrite).saveAsTable("hive_records")
+      // After insertion, the Hive managed table has data now
+      sql("SELECT * FROM hive_records").show()
     }
-    stringsDS.show()
-
-    // You can also use DataFrames to create temporary views within a SparkSession.
-    val recordsDF = spark.createDataFrame((1 to 100).map(i => Record(i, s"val_$i")))
-    recordsDF.createOrReplaceTempView("records")
-
-    // Queries can then join DataFrame data with data stored in Hive.
-    sql("SELECT * FROM records r JOIN src s ON r.key = s.key").show()
-
-    // `USING hive`
-    if(!spark.catalog.tableExists("hive_records")){
-      sql("drop table hive_records")
-    sql("CREATE TABLE hive_records(key int, value string) STORED AS PARQUET")
-    }
-    // Save DataFrame to the Hive managed table
-    val df = spark.table("src")
-
-    df.write.mode(SaveMode.Overwrite).saveAsTable("hive_records")
-    // After insertion, the Hive managed table has data now
-    sql("SELECT * FROM hive_records").show()
-
 
     spark.close()
   }
